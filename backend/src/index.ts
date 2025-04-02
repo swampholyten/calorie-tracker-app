306e691f1bdd0e5b889ff266beb8db8b5a72e336
Junkai Ji <98195504+swampholyten@users.noreply.github.com>
2025-04-02 17:12:50 +0200

    add db and auth

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,12 @@
-import { Hono } from 'hono'
+import { auth } from "@/auth";
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+app.on(["POST", "GET"], "/api/auth/**", (c) => auth.handler(c.req.raw));
+
+export default app;
